927b482f3a3afa5eebb70afc8df8ea51d3151233
jk <jferna57@gmail.com>
2016-09-30 16:01:43 +0200

    add Contract i18n message.

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -24,6 +24,7 @@
             resolve: {
                 mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
                     $translatePartialLoader.addPart('home');
+                    $translatePartialLoader.addPart('contract');
                     return $translate.refresh();
                 }]
             }
