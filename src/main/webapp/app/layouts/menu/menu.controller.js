17817da41a66b1ba34c9ea18cc68ccc59fe33536
jk <jferna57@gmail.com>
2016-10-14 15:35:07 +0200

    Add menu on the left.

---
+++ src/main/webapp/app/layouts/menu/menu.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('pocSignaturitApp')
+        .controller('MenuController', MenuController);
+
+    MenuController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+
+    function MenuController ($scope, Principal, LoginService, $state) {
+        var vm = this;
+
+
+    }
+})();
